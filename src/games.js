0eb05236fddc79e7224510829561f108b1e3193b
Alan Luo <i@iltc.io>
2021-04-05 23:40:23 -0400

    Add Games

---
+++ src/games.js
@@ -0,0 +1,103 @@
+export default {
+    data: [
+        {
+            'title': 2021,
+            'items': [
+                {
+                    'title': 'Marvel\'s Spider-Man',
+                    'link': 'https://www.playstation.com/en-us/games/marvels-spider-man/',
+                    'image': 'images/SpiderMan.jpeg',
+                    'author': 'Insomniac Games'
+                },
+                {
+                    'title': 'Cyberpunk 2077',
+                    'link': 'https://www.cyberpunk.net/',
+                    'image': 'images/Cyberpunk2077.jpeg',
+                    'author': 'CD Projekt'
+                }
+            ]
+        },
+        {
+            'title': 2020,
+            'items': [
+                {
+                    'title': 'Watch Dogs: Legion',
+                    'link': 'https://www.ubisoft.com/en-us/game/watch-dogs/legion',
+                    'image': 'images/WatchDogsLegion.webp',
+                    'author': 'Ubisoft'
+                },
+                {
+                    'title': 'Watch Dogs 2',
+                    'link': 'https://www.ubisoft.com/en-us/game/watch-dogs/watch-dogs-2',
+                    'image': 'images/WatchDogs2.jpeg',
+                    'author': 'Ubisoft'
+                },
+                {
+                    'title': '隐形守护者',
+                    'link': 'https://zh.wikipedia.org/wiki/%E9%9A%90%E5%BD%A2%E5%AE%88%E6%8A%A4%E8%80%85',
+                    'image': 'images/TheInvisibleGuardian.jpeg',
+                    'author': 'New One Studio'
+                },
+                {
+                    'title': 'Rise of the Tomb Raider',
+                    'link': 'https://en.wikipedia.org/wiki/Rise_of_the_Tomb_Raider',
+                    'image': 'images/RiseOfTheTombRaider.jpeg',
+                    'author': 'Crystal Dynamics'
+                },
+                {
+                    'title': 'Uncharted: The Lost Legacy',
+                    'link': 'https://www.unchartedthegame.com/en-us/games/uncharted-the-lost-legacy/',
+                    'image': 'images/UnchartedTheLostLegacy.jpeg',
+                    'author': 'Naughty Dog'
+                },
+                {
+                    'title': 'Uncharted 4: A Thief’s End',
+                    'link': 'https://www.unchartedthegame.com/en-us/games/uncharted-4/',
+                    'image': 'images/Uncharted4.jpeg',
+                    'author': 'Naughty Dog'
+                },
+                {
+                    'title': 'Watch Dogs: Bad Blood',
+                    'link': 'https://en.wikipedia.org/wiki/Watch_Dogs',
+                    'image': 'images/WatchDogsBadBlood.webp',
+                    'author': 'Ubisoft'
+                },
+                {
+                    'title': 'Watch Dogs',
+                    'link': 'https://en.wikipedia.org/wiki/Watch_Dogs',
+                    'image': 'images/WatchDogs.webp',
+                    'author': 'Ubisoft'
+                },
+                {
+                    'title': 'Tomb Raider',
+                    'link': 'https://en.wikipedia.org/wiki/Tomb_Raider',
+                    'image': 'images/TombRaider.jpeg',
+                    'author': 'Crystal Dynamics'
+                }
+            ]
+        },
+        {
+            'title': 2016,
+            'items': [
+                {
+                    'title': 'Shadow Complex',
+                    'link': 'https://www.epicgames.com/shadowcomplex/',
+                    'image': 'images/ShadowComplex.jpeg',
+                    'author': 'Chair Entertainment'
+                },
+                {
+                    'title': 'Quantum Break',
+                    'link': 'https://www.remedygames.com/games/quantumbreak/',
+                    'image': 'images/QuantumBreak.jpeg',
+                    'author': 'Remedy Entertainment'
+                },
+                {
+                    'title': 'Firewatch',
+                    'link': 'https://www.firewatchgame.com/',
+                    'image': 'images/Firewatch.jpeg',
+                    'author': 'Campo Santo'
+                }
+            ]
+        }
+    ]
+}
